349c1e2093ee29b626edcc599c54cf9ba739176f
Andrew <71615580+Jockesss@users.noreply.github.com>
2024-03-07 19:02:47 +0900

    Added FindNonDuplicateNumberInstances.go

---
+++ Tasks/TwoPointers/FindNonDuplicateNumberInstances.go
@@ -0,0 +1,41 @@
+/*
+	Given an array of sorted numbers, move all non-duplicate number instances at the beginning of the array in-place.
+	The relative order of the elements should be kept the same and you should not use any extra space so
+	that the solution has constant space complexity i.e., .
+
+	Move all the unique number instances at the beginning of the array and
+	after moving return the length of the subarray that has no duplicate in it.
+
+	Учитывая массив отсортированных чисел, переместите все недублирующиеся экземпляры чисел в начало массива на место.
+	Относительный порядок элементов должен оставаться неизменным, и вы не должны использовать лишнее пространство,
+	чтобы решение имело постоянную пространственную сложность, т.е. .
+
+	Переместите все уникальные экземпляры чисел в начало массива и
+	после перемещения верните длину подмассива, в котором нет дубликатов.
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{2, 3, 3, 3, 6, 9, 9}
+	fmt.Println(arr)
+}
+
+func remove(arr []int) int {
+	m := make(map[int]bool)
+	count := 0
+	//start := 0
+	//end := len(arr) - 1
+
+	for i := 0; i < len(arr); i++ {
+		if _, ok := m[arr[i]]; ok {
+			count++
+		}
+
+	}
+
+	return count
+}
