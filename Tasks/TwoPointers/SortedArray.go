c44b0bce5c1248747d20b02e43bf7855e49bff02
Andrew <71615580+Jockesss@users.noreply.github.com>
2024-03-11 19:12:30 +0900

    Added SortedArray.go

---
+++ Tasks/TwoPointers/SortedArray.go
@@ -0,0 +1,19 @@
+/*
+Given a sorted array, create a new array containing
+squares of all the numbers of the input array in the sorted order.
+
+Задав отсортированный массив, создайте новый массив,
+содержащий квадраты всех чисел входного массива в отсортированном порядке.
+*/
+package main
+
+func main() {
+
+}
+
+func makeSquares(arr []int) []int {
+	n := len(arr)
+	squares := make([]int, n)
+	// TODO: Write your code here
+	return squares
+}
