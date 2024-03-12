cd6d764c93fcbb05b2c61d816694adf6583a3721
Andrew <71615580+Jockesss@users.noreply.github.com>
2024-03-12 18:38:52 +0900

    Added SortedArray.go

--- Tasks/TwoPointers/SortedArray.go
+++ Tasks/TwoPointers/SortedArray.go
@@ -7,13 +7,36 @@ squares of all the numbers of the input array in the sorted order.
 */
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	arr := []int{-4, -1, 0, 3, 10}
+	sortedArr := makeSquares(arr)
+	fmt.Println(sortedArr) // Вывод: [0 1 9 16 100]
 }
 
 func makeSquares(arr []int) []int {
 	n := len(arr)
 	squares := make([]int, n)
-	// TODO: Write your code here
+
+	left, right := 0, n-1
+
+	for i := n - 1; i >= 0; i-- {
+		if abs(arr[left]) > abs(arr[right]) {
+			squares[i] = arr[left] * arr[left]
+			left++
+		} else {
+			squares[i] = arr[right] * arr[right]
+			right--
+		}
+	}
+
 	return squares
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
