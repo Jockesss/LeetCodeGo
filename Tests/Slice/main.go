10152b5d77ebfd1ca0d3453e780298cc31c31bdd
Jockess <jockes.1998@gmail.com>
2024-05-06 15:38:02 +0900

    Slice tasks added

--- Tests/Slice/main.go
+++ Tests/Slice/main.go
@@ -12,8 +12,7 @@ package main
 import "fmt"
 
 func main() {
-	task1()
-	task2()
+	task5()
 }
 
 func task1() {
@@ -22,7 +21,8 @@ func task1() {
 	sl[1] = 10
 
 	fmt.Println(sl)
-	fmt.Println(arr, "____________________")
+	fmt.Println(arr)
+	fmt.Println("____________________")
 }
 
 func task2() {
@@ -31,7 +31,8 @@ func task2() {
 	sl[1] = 10
 
 	fmt.Println(sl)
-	fmt.Println(arr, "____________________")
+	fmt.Println(arr)
+	fmt.Println("____________________")
 }
 
 func task3() {
@@ -40,5 +41,34 @@ func task3() {
 	sl = append(sl, 4)
 
 	fmt.Println(sl)
-	fmt.Println(arr, "____________________")
+	fmt.Println(arr)
+	fmt.Println("____________________")
+}
+
+func task4() {
+	arr := [5]int{1, 2, 3, 4, 5}
+	sl := arr[1:2]
+	sl2 := append(sl, 4)
+	sl2[0] = 8
+	fmt.Println(arr)
+	fmt.Println(sl)
+	fmt.Println(sl2)
+	fmt.Println("____________________")
+}
+
+func task5() {
+	arr := [5]int{1, 2, 3, 4, 5}
+	sl := arr[1:2]
+	sl = append(sl, 9)
+	sl = append(sl, 8)
+	sl = append(sl, 7)
+	fmt.Println(arr)
+
+	sl = append(sl, 6)
+	fmt.Println(arr)
+
+	sl[0] = 0
+	sl[1] = 0
+
+	fmt.Println(sl)
 }
